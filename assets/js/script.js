76fd64ea3e381f7c69657669618b9ce571175399
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-09-16 16:07:08 -0600

    Questions and answeres added

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const QuizQs = [
+    {
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        answers: ["getElementbyId()", "getElementsByName()", "getElementsbyElement()",],
+        correctAnswer: "getElementbyId()"
+    },
+
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        answers: ["get", "set", "let"],
+        correctAnswer: "let"
+    },
+
+    {
+        question: "What function is used to convert a JSON object into string format?",
+        answers: ["parse()", "stringit()", "stringify()",],
+        correctAnswer: "stringify()"
+    },
+
+    {
+        question: "How do you write a comment in javascript?",
+        answers: ["<!-- -->", "/* */", "//",],
+        correctAnswer: "//"
+    },
+];
